7e31aa9e05e52fb5450372f88cf3fe4af79422d9
Lars Niesen <lars.niesen@gmx.de>
2023-04-23 11:26:00 +0200

    Fix uid mapping

--- dev/runonce_docker.sh
+++ dev/runonce_docker.sh
@@ -7,11 +7,3 @@ set -o nounset
 USERNAME="$(logname)"
 echo "${USERNAME}:$(id -u ${USERNAME}):1" >> /etc/subuid
 echo "${USERNAME}:$(id -g ${USERNAME}):1" >> /etc/subgid
-
-[ ! -d /etc/docker ] && mkdir /etc/docker
-touch /etc/docker/daemon.json
-cat<<EOF >> /etc/docker/daemon.json
-{
-  "userns-remap": "default"
-}
-EOF
